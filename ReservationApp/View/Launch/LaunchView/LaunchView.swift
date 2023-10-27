f1cef3fa4ef0369db93ada005003b58bffda02f2
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2023-10-27 21:48:42 +0200

    feat: LaunchView initialized

---
+++ ReservationApp/View/Launch/LaunchView/LaunchView.swift
@@ -0,0 +1,27 @@
+//
+//  LaunchView.swift
+//  ReservationApp
+//
+//  Created by Thibault GODEFROY on 27/10/2023.
+//
+
+import SwiftUI
+
+// MARK: - LaunchView
+
+struct LaunchView: View {
+    // MARK: Properties
+
+    // MARK: Body
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+// MARK: - Preview
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
