8936187dbd1676e4bccb8ecf46806b3989cc2b0e
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2023-10-27 18:23:33 +0200

    Initial Commit

---
+++ ReservationApp/ReservationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationAppApp.swift
+//  ReservationApp
+//
+//  Created by Thibault GODEFROY on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReservationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
