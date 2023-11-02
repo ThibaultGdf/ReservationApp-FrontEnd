83bf0be205448c3a557b150b55674c8c107fd7de
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2023-11-02 14:05:38 +0100

    feat: RadioButtonComponent updated

--- ReservationApp/Components/RadioButtonComponent.swift
+++ ReservationApp/Components/RadioButtonComponent.swift
@@ -7,14 +7,54 @@
 
 import SwiftUI
 
+// MARK: - RadioButtonComponent
+
 struct RadioButtonComponent: View {
+    // MARK: Properties
+
+    @State private var isTable: Bool = false
+    @State private var isSalle: Bool = false
+    @State private var isOn: Bool = false
+
+    var size: CGFloat = 20
+    var text: String = ""
+    // MARK: Body
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            radioButton(text: "Table", isOn: $isTable)
+            radioButton(text: "Salle", isOn: $isSalle)
+        }
+    }
+    func radioButton(text: String, isOn: Binding<Bool>) -> some View {
+        Button {
+            withAnimation {
+                            isOn.wrappedValue.toggle()
+                        }
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(maxWidth: size, maxHeight: size)
+                    .foregroundColor(.white)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.black, lineWidth: 1)
+                    )
+                if isOn.wrappedValue {
+                    Circle()
+                        .frame(maxWidth: size / 2, maxHeight: size / 2)
+                        .foregroundColor(.greenDark)
+                }
+            }
+            TextComponent(text: text)
+                .foregroundColor(.basicColor)
+        }
     }
 }
+// MARK: Preview
 
 struct RadioButtonComponent_Previews: PreviewProvider {
     static var previews: some View {
-        RadioButtonComponent()
+        RadioButtonComponent(text: "Table")
     }
 }
